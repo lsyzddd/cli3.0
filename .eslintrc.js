7c082733055d2b34354c2c56edbb5a4e45fa5091
lsy <943841170@qq.com>
2019-02-03 14:35:39 +0800

    cli3.0创建

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,12 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-use-before-define": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-mixed-spaces-and-tabs": [2, false]
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
+    ecmaVersion: 6
   }
 };
